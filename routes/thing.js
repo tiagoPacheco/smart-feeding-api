943d426166f44d4ba25d91818aae7f11e0fca616
Rafael Lucena <rafael_lucena@apple.com>
2018-09-15 10:05:20 -0300

    Added the "feed_pet" route introduction

--- routes/thing.js
+++ routes/thing.js
@@ -3,6 +3,14 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var router = express.Router();
 
+var KNoTCloud = require('knot-cloud');
+// let cloud = new KNoTCloud(
+//   'knot-test.cesar.org.br',
+//   3000,
+//   '81855be9-3c51-4746-98cd-0dffaacd0000',
+//   '6c9deba3f1fac2df20e3a6284469d2dbf502c5eb',
+// );
+
 var Thing = require('../models/thing');
 
  router.use(bodyParser.json());
@@ -10,9 +18,9 @@ var Thing = require('../models/thing');
 router.route('/')
 .get(function(req, res, next) {
   Thing.find({}, function(err, thing){
-    if(err) throw err;  
+    if(err) throw err;
     res.json(thing);
-  }); 
+  });
 })
 
 .post(function (req, res, next){
@@ -28,20 +36,46 @@ router.route('/')
   });
 });
 
+router.route('/feed_pet')
+.post(function (req, res, next) {
+  res.json({name: "Testing"})
+
+  // try {
+  //   await cloud.connect();
+  //   const devices = cloud.;
+  //   console.log(devices);
+  // } catch (err) {
+  //   console.error(err);
+  // }
+
+  // await cloud.close();
+
+  // Thing.create(req.body, function(err, thing){
+  //   if(err) throw err;
+  //   console.log('Thing Created!');
+  //   var id = thing._id;
+  //
+  //   res.writeHead(200, {
+  //       'Content-Type': 'text/plain'
+  //   });
+  //   res.end(String(id));
+  // });
+});
+
 router.route('/:userId/')
 .get(function(req, res, next) {
   Thing.find({ userId: req.params.userId}, function(err, thing){
-    if(err) throw err;  
+    if(err) throw err;
     res.json(thing);
-  }); 
+  });
 });
 
 router.route('/update/:thingId/')
 .put(function(req, res, next) {
   Thing.findByIdAndUpdate(req.params.thingId, { $set: req.body }, { new: true }, function(err, thing){
-    if(err) throw err;  
+    if(err) throw err;
     res.json(thing);
-  }); 
+  });
 });
 ;
 
